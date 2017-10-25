7e30a3214044b2f7641cfdb5ba94ff92b391a6fb
BhargavBV <bhargavauden1998@gmail.com>
2017-10-25 23:00:47 +0530

    ProfileService

---
+++ app/src/main/java/com/example/bhargavbv/eventmanager/ProfileActivity_Service.java
@@ -0,0 +1,13 @@
+package com.example.bhargavbv.eventmanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProfileActivity_Service extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile__service);
+    }
+}
